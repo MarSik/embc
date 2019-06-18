81b8b76ddb968872797b0f265a4679ed2fe2c733
Martin Sivák <mars@montik.net>
2019-06-18 22:29:25 +0200

    Bump cmake version

--- embedded_cmake/environment.py
+++ embedded_cmake/environment.py
@@ -15,6 +15,6 @@ TOOLCHAINS_DIR = os.path.join(CMAKE_SCRIPTS, "Toolchains")
 TEMPLATE_DIR = os.path.join(CMAKE_SCRIPTS, "Templates")
 TEMPLATE_SUFFIX = ".j2"
 
-CMAKE_PACKAGE = "https://cmake.org/files/v3.8/cmake-3.8.1-Linux-x86_64.tar.gz"
+CMAKE_PACKAGE = "https://github.com/Kitware/CMake/releases/download/v3.14.0-rc2/cmake-3.14.0-rc2-Linux-x86_64.tar.gz"
 
 PROJECT_LIB = "lib"
